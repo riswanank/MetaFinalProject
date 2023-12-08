983e93739de2079d2dfdec6daeea448413a36ac9
riswanank <38204116+riswanank@users.noreply.github.com>
2023-12-08 11:20:11 +0400

    initial html setup

--- littlelemon/src/Header.js
+++ littlelemon/src/Header.js
@@ -3,6 +3,10 @@ function Header ()
     return (
         <HTMLHeadElement>
             <title>Little Lemon Restaurant</title>
+            <meta name="description" content="Website of Little Lemon Restaurant"/>
+            <meta name="og:title" content="Little Lemon Restaurant"/>
+            <meta name="og:description" content="Little Lemon Restaurants Website to order food online"/>
+            <meta name="og:image" content=""/>
         </HTMLHeadElement>
 
     );
